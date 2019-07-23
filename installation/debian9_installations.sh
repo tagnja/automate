f3d8c56d68cd0f39d9a2967a810b8dcfa0c25c92
taogen <taogenjia@gmail.com>
2019-07-23 11:09:47 +0800

    Little update

--- installation/debian9_installations.sh
+++ installation/debian9_installations.sh
@@ -2,59 +2,71 @@
 
 function install_redis
 {
-
-	echo "**************************"
-	echo "install redis begin..."
-
+	echo ""
+	echo ""
+	echo "install redis begin<<<<<<<<<<<<<<<<<<<<"
+	echo ""
+	echo ""
+	
 	# download
 	echo ""	
-	echo "download<<<<<<<<<<<<<<<<<<"
+	echo "download..."
 	echo ""
 	wget http://download.redis.io/releases/redis-5.0.5.tar.gz
 
 	
 	# unzip	
 	echo ""
-	echo "unzip<<<<<<<<<<<<<<<<<<<<<"
+	echo "unzip..."
 	echo ""
 	install_path=/tools
-	mkdir -p $install_path
+	sudo mkdir -p $install_path
 	sudo tar -xzvf redis-5.0.5.tar.gz -C $install_path
 	
 	
 	# make
 	echo ""
-	echo "make<<<<<<<<<<<<<<<<<<<<<<"
+	echo "make..."
 	echo ""
 	cd $install_path/redis-5.0.5
-	make
+	sudo make
 
 	# result
 	echo ""
 	echo ""
 	echo "Install redis is successful! ( Start redis using $ cd /tools/redis-5.0.5; ./src/redis-server; )"
-	echo "**************************"
+	echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+	echo "";
+	echo "";
 
 
 }
 
-echo "******************************"
+# main
+
+echo "************************************************************"
 echo "begin installation..."
 echo ""
-
+echo ""
 
 if ! [ -x "$(command -v sudo)" ]; then
-	echo ""
+    echo ""
     echo "install sudo<<<<<<<<<<<<<<<<<<<<"
-	echo ""
+    echo ""
     apt-get install sudo -y
+    echo ""
+    echo "install sudo is successful! >>>>>>>>>>>>>>>>>>>>>>"
+    echo ""
 fi
 
 if ! [ -x "$(command -v make)" ]; then
-	echo ""
+    echo ""
     echo "install make<<<<<<<<<<<<<<<<<<<<"
-	echo ""
+    echo ""
     sudo apt-get install build-essential -y
+    echo ""
+    echo "install make is successful! >>>>>>>>>>>>>>>>>>>>>>"
+    echo ""
 fi
 
 install_redis
@@ -62,6 +74,8 @@ install_redis
 echo ""
 echo ""
 echo "All installation process is over!"
-echo "******************************"
+echo "************************************************************"
+echo ""
+echo ""
 
 exit
