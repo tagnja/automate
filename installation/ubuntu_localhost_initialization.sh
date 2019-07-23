37cfb1f12f8016b49b85ee03ebf16f595d9d3ff5
taogen <taogenjia@gmail.com>
2019-07-23 18:10:23 +0800

    Initial commit

---
+++ installation/ubuntu_localhost_initialization.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+echo "
+deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse
+"| sudo tee /etc/apt/sources.list  # add -a for append (>>) ;
+
+
+sudo apt-get update
+sudo apt-get upgrade
