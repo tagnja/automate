397190b8f431a1cbbc4a68f264139d100ce8e0d3
taogen <taogenjia@gmail.com>
2019-07-23 09:32:58 +0800

    Update

--- installation/debian9_installations.sh
+++ installation/debian9_installations.sh
@@ -5,8 +5,8 @@ install_redis () {
 echo "install redis begin..."
 wget http://download.redis.io/releases/redis-5.0.5.tar.gz
 install_path=/tools
-if [ ! $jdk_path ]; then
-	mkdir /tools
+if [ ! $install_path ]; then
+	sudo mkdir /tools
 	return
 fi
 tar -xzvf redis-5.0.5.tar.gz -C /tools/
