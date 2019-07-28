9bb6790f03eb5810344eb0ec76434061469133c0
taogen <taogenjia@gmail.com>
2019-07-28 09:06:32 +0800

    initial commit

---
+++ network/pull_build_and_deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v git)" ]; then
+	echo -e "\n\ninstall git...\n\n"
+	sudo apt-get install git -y
+	echo -e "\n\ngit installation is successful!\n\n"
+fi
+
+if ! [ -x "$(command -v mvn)" ]; then
+	echo -e "\n\ninstall git...\n\n"
+	sudo apt-get install maven -y
+	echo -e "\n\ngit installation is successful!\n\n"
+fi
+
+git clone https://github.com/tagnja/hot-crawler.git
+cd hot-crawler
+mvn package spring-boot:repackage
+if [[ -n $(pgrep -f java) ]]
+	pid=$(pgrep -f java | head -n 1)
+	echo -e "\n\nkill pid $pid\n\n"
+	sudo kill -9 $pid
+	echo -e "\n\nkill pid $pid is successful!\n\n"
+fi
+java -jar ./target/hotcrawler-1.0-SNAPSHOT.jar &
