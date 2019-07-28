4bde8174dca85224ef7fbc4a06d5f54f6df138a7
taogen <taogenjia@gmail.com>
2019-07-28 09:27:41 +0800

    Little update

--- network/pull_build_and_deploy.sh
+++ network/pull_build_and_deploy.sh
@@ -5,15 +5,15 @@ proj_name=hot-crawler
 target_name=hotcrawler
 
 if ! [ -x "$(command -v git)" ]; then
-	echo -e "\n\ninstall git...\n\n"
+	echo -e "\n\nInstall git...\n\n"
 	sudo apt-get install git -y
-	echo -e "\n\ngit installation is successful!\n\n"
+	echo -e "\n\nGit installation is successful!\n\n"
 fi
 
 if ! [ -x "$(command -v mvn)" ]; then
-	echo -e "\n\ninstall maven...\n\n"
+	echo -e "\n\nInstall maven...\n\n"
 	sudo apt-get install maven -y
-	echo -e "\n\nmaven installation is successful!\n\n"
+	echo -e "\n\nMaven installation is successful!\n\n"
 fi
 
 
@@ -22,16 +22,22 @@ if ! [[ -n $(ls $proj_name) ]]; then
 fi
 
 cd $proj_name
+
+echo -e "\n\nPull project...\n\n"
 git pull
+echo -e "\n\nPull project is successful!\n\n"
+
+echo -e "\n\nBuild project...\n\n"
 mvn package spring-boot:repackage
+echo -e "\n\nBuild project is successful!\n\n"
 
 if [[ -n $(pgrep -f java) ]]; then
 	pid=$(pgrep -f java | head -n 1)
-	echo -e "\n\nkill pid $pid\n\n"
+	echo -e "\n\nTO kill pid $pid...\n\n"
 	sudo kill -9 $pid
-	echo -e "\n\nkill pid $pid is successful!\n\n"
+	echo -e "\n\nKill pid $pid is successful!\n\n"
 fi
 
-echo -e "\n\nstart project..\n\n"
+echo -e "\n\nStart project...\n\n"
 java -jar ./target/$target_name-1.0-SNAPSHOT.jar &
-
+echo -e "\n\nStart project is successful!\n\n"
