a77cb74f61d652eb70f587d433b5f48caffb871f
taogen <taogenjia@gmail.com>
2019-07-28 09:11:15 +0800

    update

--- network/pull_build_and_deploy.sh
+++ network/pull_build_and_deploy.sh
@@ -7,18 +7,24 @@ if ! [ -x "$(command -v git)" ]; then
 fi
 
 if ! [ -x "$(command -v mvn)" ]; then
-	echo -e "\n\ninstall git...\n\n"
+	echo -e "\n\ninstall maven...\n\n"
 	sudo apt-get install maven -y
-	echo -e "\n\ngit installation is successful!\n\n"
+	echo -e "\n\nmaven installation is successful!\n\n"
 fi
 
-git clone https://github.com/tagnja/hot-crawler.git
-cd hot-crawler
+proj_name=hot-crawler
+if ! [[ -n ls $proj_name ]]
+	git clone https://github.com/tagnja/hot-crawler.git
+fi
+
+cd $proj_name
 mvn package spring-boot:repackage
+
 if [[ -n $(pgrep -f java) ]]
 	pid=$(pgrep -f java | head -n 1)
 	echo -e "\n\nkill pid $pid\n\n"
 	sudo kill -9 $pid
 	echo -e "\n\nkill pid $pid is successful!\n\n"
 fi
+
 java -jar ./target/hotcrawler-1.0-SNAPSHOT.jar &
