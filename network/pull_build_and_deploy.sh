f243f037eb788e643909bd6a89f8749d6414ded6
taogen <taogenjia@gmail.com>
2019-07-28 10:54:24 +0800

    Littile update

--- network/pull_build_and_deploy.sh
+++ network/pull_build_and_deploy.sh
@@ -3,49 +3,82 @@
 github_user=tagnja
 proj_name=hot-crawler
 target_name=hotcrawler
+package_manager=apt-get
+server_location=inland
 
-if [ -x "$(command -v apt-get)" ]; then
-	package_manager=apt-get
-else
+# check package manager
+echo -e "\n\n Check package manager... \n\n"
+if [ -x "$(command -v yum)" ]; then
 	package_manager=yum
 fi
-echo -e "\n\nPackage manager is $package_manager.\n\n"
+echo -e "\n\n Package manager is $package_manager. \n\n"
 
+# check server location
+echo -e "\n\n Check server location... \n\n"
+if [ -x "$(ping -c 1 google.com &> /dev/null)" ]; then 
+	server_location=abroad
+fi
+echo -e "\n\n Server location is $server_location. \n\n"
+
+
+# install JDK
+if ! [ -x "$(command -v java)" ]; then
+	echo -e "\n\n Install JDK... \n\n"
+	sudo $package_manager install java
+	echo -e "\n\n JDK installation is successful! \n\n"
+fi
+
+
+# install Git
 if ! [ -x "$(command -v git)" ]; then
-	echo -e "\n\nInstall git...\n\n"
+	echo -e "\n\n Install git... \n\n"
 	sudo $package_manager install git -y
-	echo -e "\n\nGit installation is successful!\n\n"
+	echo -e "\n\n Git installation is successful! \n\n"
 fi
 
+# install Maven
 if ! [ -x "$(command -v mvn)" ]; then
-	echo -e "\n\nInstall maven...\n\n"
+	echo -e "\n\n Install maven... \n\n"
 	sudo $package_manager install maven -y
-	echo -e "\n\nMaven installation is successful!\n\n"
+	echo -e "\n\n Maven installation is successful! \n\n"
+fi
+
+# update Maven conf by server_location
+
+
+
+# install redis
+if ! [ -x "$(service --status-all | grep -Fq redis)" ]; then
+	echo -e "\n\n Install Redis... \n\n"
+	sudo $package_manager install redis -y
+	echo -e "\n\n Redis installation is successful! \n\n"
 fi
 
 
+# git clone project
 if ! [[ -n $(ls $proj_name) ]]; then
-	echo -e "\n\nGit clone...\n\n"
+	echo -e "\n\n Git clone... \n\n"
 	git clone https://github.com/$github_user/$proj_name.git
-	echo -e "\n\nGit clone is successful!\n\n"
+	echo -e "\n\n Git clone is successful! \n\n"
 fi
 
+# git pull project
 cd $proj_name
-
-echo -e "\n\nPull project...\n\n"
+echo -e "\n\n Pull project... \n\n"
 git pull
-echo -e "\n\nPull project is successful!\n\n"
+echo -e "\n\n Pull project is successful! \n\n"
 
-echo -e "\n\nBuild project...\n\n"
+# build project
+echo -e "\n\n Build project... \n\n"
 mvn package spring-boot:repackage
-echo -e "\n\nBuild project is successful!\n\n"
+echo -e "\n\n Build project is finished! \n\n"
 
+# start project
 if [[ -n $(pgrep -f java) ]]; then
 	pid=$(pgrep -f java | head -n 1)
-	echo -e "\n\nTo kill pid $pid...\n"
+	echo -e "\n\n To kill pid $pid... \n"
 	sudo kill -9 $pid
-	echo -e "Kill pid $pid is successful!\n\n"
+	echo -e "\n\n Kill pid $pid is successful! \n\n"
 fi
-
-echo -e "\n\nStart project...\n\n"
+echo -e "\n\n Start project... \n\n"
 java -jar ./target/$target_name-1.0-SNAPSHOT.jar &
